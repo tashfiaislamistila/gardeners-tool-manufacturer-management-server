ee39028a79c0ba983e4fc97333b2e489346b7dcb
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-24 01:29:43 -0700

    database connected in backend

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://tool_admin:<password>@cluster0.xt38z.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xt38z.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connected');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from gardeners tool management !')
