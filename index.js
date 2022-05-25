d48f827bf05fd0cc8fb60683360e2e02684476b9
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-24 21:49:20 -0700

    single tools api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -16,13 +16,20 @@ async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('gardenres_tool_management').collection('tools');
-
+        //tools API
         app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
-        })
+        });
+        //get single tools API
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        });
     }
     finally {
 
